123d98d20f68d9e78d7838540d0d0d6fc108efd3
No Segmentation Fault Work <generaluseai@gmail.com>
2021-05-20 22:18:09 +0800

    完成了User类的基本结构，还有几个todo要做

--- src/ir/values/User.java
+++ src/ir/values/User.java
@@ -1,10 +1,34 @@
 package ir.values;
 
+import ir.Use;
 import ir.types.Type;
+import java.util.ArrayList;
 
-public class User extends Value {
+/**
+ * User对象，记录了自身操作的 Value 的状态 要记住的一点是 User 的 Operand 和 Use 类表达的是同一个东西， 只是为了处理方便同时做了两种设计，需要保持 Operand
+ * 和 Use 的一致
+ */
+public abstract class User extends Value {
 
-  public User(Type type) {
-    super(type);
+  public User(String name, Type type, int numOP) {
+    super(name, type);
+    this.numOP = numOP;
   }
+
+  public Value getOperand(int i) {
+    return operands.get(i);
+  }
+
+  /**
+   * 将自己从所有Operand的Uselist中删除
+   */
+  public void removeUsesOfOPs() {
+    for (Value operand : operands) {
+      //todo 加方法
+    }
+  }
+
+  private ArrayList<Value> operands;
+  private int numOP;
 }
+//todo
