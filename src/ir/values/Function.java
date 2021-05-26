d38c1e2eeb95146d3651888d90c5897c403f1f58
No Segmentation Fault Work <generaluseai@gmail.com>
2021-05-26 23:24:48 +0800

    修改了Function的内部类Arg

--- src/ir/values/Function.java
+++ src/ir/values/Function.java
@@ -5,24 +5,27 @@ import ir.types.Type;
 import java.util.ArrayList;
 
 /**
- * Function类,代表一个函数，拥有一串基本块，一串参数，一个SymbolTable //todo 考虑函数内联
+ * Function类,代表一个函数，拥有一串基本块，一串参数，一个SymbolTable //
  */
-public class Function extends User {
+public class Function {
 
   //参数声明，不含值
   public class Arg extends Value {
+
     //todo 做对函数内联方便的架构设计
-
-
     private int rank;//排第几,非负
 
-    public Arg(String name, Type type) {
-      super(name, type);
+    public Arg(Type type, int rank) {
+      super("", type);
+      this.rank = rank;
     }
   }
 
-  public Function(String name, Type type, int numOP) {
-    super(name, type, numOP);
+  public ArrayList<BasicBlock> getBasicBlocks() {
+    return basicBlocks;
+  }
+
+  public Function() {
   }
 
   private boolean isBuiltin;//lib function
