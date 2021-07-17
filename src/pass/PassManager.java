4e85dec94c9a5f38fd8aa08941861f01bc1ad834
Codevka <1724707236@qq.com>
2021-07-17 15:29:59 +0800

    bbpredsucc first version

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -5,6 +5,7 @@ import ir.MyModule;
 import java.util.ArrayList;
 import pass.Pass.IRPass;
 import pass.Pass.MCPass;
+import pass.ir.BBPredSucc;
 import pass.ir.EmitLLVM;
 import pass.ir.GVNGCM;
 import pass.ir.Mem2reg;
@@ -15,6 +16,7 @@ public class PassManager {
   private static PassManager passManager = new PassManager();
   private ArrayList<String> openedPasses_ = new ArrayList<>() {{
     add("typeCheck");
+    add("bbPredSucc");
     add("Mem2reg");
     add("emitllvm");
 //    add("gvngcm");
@@ -26,6 +28,7 @@ public class PassManager {
 
   private PassManager() {
     //pass执行的顺序在这里决定,如果加了而且是open的，就先加的先跑
+    irPasses.add(new BBPredSucc());
     irPasses.add(new Mem2reg());
     irPasses.add(new EmitLLVM());
 //    irPasses.add(new GVNGCM());
