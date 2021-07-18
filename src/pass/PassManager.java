9cad1fb7cad8c7bae8989b72b0b632426912ef74
dglr <dglr104524563@gmail.com>
2021-07-18 17:04:45 +0800

    passmanager cnm

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -41,12 +41,11 @@ public class PassManager {
     //pass执行的顺序在这里决定,如果加了而且是open的，就先加的先跑
     irPasses.add(new BBPredSucc());
     irPasses.add(new Mem2reg());
-    irPasses.add(new BranchOptimization());
-//    irPasses.add(new EmitLLVM());
-    irPasses.add(new InterproceduralAnalysis());
-    irPasses.add(new GVNGCM());
-    irPasses.add(new DeadCodeEmit());
     irPasses.add(new EmitLLVM());
+    irPasses.add(new InterproceduralAnalysis());
+//    irPasses.add(new GVNGCM());
+    // irPasses.add(new DeadCodeEmit());
+//    irPasses.add(new EmitLLVM());
 
 //    mcPasses.add(new RegAllocator());
 
