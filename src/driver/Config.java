148389c881f56953d704750ae8e8a1797330a9e8
generaluseai <generaluseai@gmail.com>
2021-07-19 17:08:05 +0800

    [fix] removed useless import file

--- src/driver/Config.java
+++ src/driver/Config.java
@@ -7,7 +7,6 @@ import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import net.sourceforge.argparse4j.inf.Namespace;
 import util.Mylogger;
 
 /***
