f0f81a7b376691cf648f7480503358c705e8739b
generaluseai <generaluseai@gmail.com>
2021-07-19 17:31:37 +0800

    [fix] UnaryInst->add property

--- src/ir/values/instructions/UnaryInst.java
+++ src/ir/values/instructions/UnaryInst.java
@@ -1,10 +1,20 @@
 package ir.values.instructions;
 
+import ir.types.IntegerType;
 import ir.types.Type;
 import ir.values.BasicBlock;
+import ir.values.Value;
 
 public class UnaryInst extends Instruction {
 
+
+  //llvm 里没有 Not Inst , 是用icmp ne 0 和 xor 进行的转换
+  //在这里的name2是为了导出llvm ir 时保证语法正确，用来debug
+  private String name2;
+
+  public void setName2(String name2) {
+    this.name2 = name2;
+  }
 
   public UnaryInst(TAG_ tag, Type type, int numOP) {
     super(tag, type, numOP);
@@ -16,13 +26,19 @@ public class UnaryInst extends Instruction {
 
   public static class NotInst extends UnaryInst {
 
-    public NotInst(TAG_ tag, Type type, int numOP) {
-      super(TAG_.Not, type, numOP);
+    public NotInst(Value source) {
+      super(TAG_.Not, IntegerType.getI1(), 1);
     }
 
-    public NotInst(TAG_ tag, Type type, int numOP, BasicBlock parent) {
-      super(TAG_.Not, type, numOP, parent);
+    public NotInst(Value source, BasicBlock parent) {
+      super(TAG_.Not, IntegerType.getI1(), 1, parent);
+      this.CoSetOperand(0, source);
     }
   }
 
+  @Override
+  public String toString() {
+    // TODO: 2021/7/19
+    return null;
+  }
 }
