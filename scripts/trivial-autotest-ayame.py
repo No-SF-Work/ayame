a4d8b2f721161cd14dfd6ad18361d26a98974192
roife <roifewu@gmail.com>
2021-07-22 00:49:18 +0800

    [fix] trivial ci

--- scripts/trivial-autotest-ayame.py
+++ scripts/trivial-autotest-ayame.py
@@ -1,13 +1,13 @@
 import subprocess
 from tester import Tester
-from presenter import Presenter
+from trivial_presenter import TrivialPresenter
 from tester import get_sy_testcases
 from pretty_print import Print_C
 
 ayame_compiler = "java -classpath src:lib/antlr4-runtime-4.8.jar:lib/argparse4j-0.9.0.jar Compiler "
 
 ayame_scheme = {"scheme": "ayame_ayame", # generate ir only
-                "frontend_instr": ayame_compiler + "-emit -o {ir} {sy}",
+                "frontend_instr": ayame_compiler + "-S {sy} -o {asm}",
                 "emit_llvm_ir": True}
 
 Print_C.print_header("[Removing old data...]\n\n")
@@ -20,5 +20,5 @@ subprocess.run("rm -rf build/log/test_result.log".split())
 tester = Tester(ayame_scheme)
 tester.test()
 
-presenter = Presenter(schemes=[ayame_scheme], testcases=get_sy_testcases)
+presenter = TrivialPresenter(schemes=["ayame_ayame"], testcases=get_sy_testcases)
 presenter.present_all_testcases()
