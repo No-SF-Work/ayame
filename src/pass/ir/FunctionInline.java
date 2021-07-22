7778fc666aeb05fc296718ff77c74629107ef456
generaluseai <generaluseai@gmail.com>
2021-07-22 23:05:40 +0800

    [todo]  func inline

--- src/pass/ir/FunctionInline.java
+++ src/pass/ir/FunctionInline.java
@@ -11,6 +11,14 @@ public class FunctionInline implements IRPass {
     return "simpleFuncInline";
   }
 
+  /*
+   * todo :
+   *  1.计算函数间调用关系图，dfs找到端点函数并且往回内联
+   *  2.挑出无递归的函数
+   *  3.替换函数参数(int 类型的声明新值，array类型的替换引用(alloca **int -> gep or others))
+   *  4.构建新基本块(函数的多个出口)
+   *
+   * */
   @Override
   public void run(MyModule m) {
 
