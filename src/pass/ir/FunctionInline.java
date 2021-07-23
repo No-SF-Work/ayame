eb954375f6d3f7a1a301db9987dcdfb4717463b3
generaluseai <generaluseai@gmail.com>
2021-07-23 17:00:04 +0800

    [todo] funcinline

--- src/pass/ir/FunctionInline.java
+++ src/pass/ir/FunctionInline.java
@@ -14,7 +14,7 @@ public class FunctionInline implements IRPass {
   /*
    * todo :
    *  1.计算函数间调用关系图，dfs找到端点函数并且往回内联
-   *  2.挑出无递归的函数
+   *  2.挑出不在调用关系中的强联通分量中的函数,将其内联至caller
    *  3.替换函数参数(int 类型的声明新值，array类型的替换引用(alloca **int -> gep or others))
    *  4.构建新基本块(函数的多个出口)
    *
