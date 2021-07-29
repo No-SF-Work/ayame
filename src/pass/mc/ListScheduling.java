0f19d756441259b918975f56a44a1e64e8de77ec
dglr <dglr104524563@gmail.com>
2021-07-29 19:40:21 +0800

    rm assert in schedul

--- src/pass/mc/ListScheduling.java
+++ src/pass/mc/ListScheduling.java
@@ -115,7 +115,6 @@ public class ListScheduling implements Pass.MCPass {
             var defs = instr.getMCDef();
             var uses = instr.getMCUse();
             assert defs.stream().allMatch(def -> def instanceof PhyReg);
-            assert uses.stream().allMatch(use -> use instanceof PhyReg);
             var curNode = new Node(instr);
             nodes.add(curNode);
 
