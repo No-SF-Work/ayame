9983c9c18cf4d059dd5db837e0f88bf5ac44713f
generaluseai <generaluseai@gmail.com>
2021-07-29 16:59:59 +0800

    [modify] restore

--- src/backend/CodeGenManager.java
+++ src/backend/CodeGenManager.java
@@ -1417,7 +1417,7 @@ public class CodeGenManager {
         if (lhsIsConst && !rhsIsConst) {
             l = ir.getOperands().get(1);
             r = ir.getOperands().get(0);
-            cond = getOppoCond(cond);
+            cond = getEqualOppCond(cond);
         } else {
             l = ir.getOperands().get(0);
             r = ir.getOperands().get(1);
