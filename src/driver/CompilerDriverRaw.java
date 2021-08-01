6effaa0c355886e28dba7a10e24a267fcf8c2d77
generaluseai <generaluseai@gmail.com>
2021-08-01 14:53:09 +0800

    [fix] cd

--- src/driver/CompilerDriverRaw.java
+++ src/driver/CompilerDriverRaw.java
@@ -79,8 +79,8 @@ public class CompilerDriverRaw {
             pm.openedPasses_.add("Mem2reg");
             pm.openedPasses_.add("RegAlloc");
             pm.openedPasses_.add("gvngcm");
+            pm.openedPasses_.add("interproceduralAnalysis");
             if (Config.getInstance().isO2) {
-                pm.openedPasses_.add("interproceduralAnalysis");
                 pm.openedPasses_.add("branchOptimization");
                 pm.openedPasses_.add("emitllvm");
                 pm.openedPasses_.add("deadcodeemit");
