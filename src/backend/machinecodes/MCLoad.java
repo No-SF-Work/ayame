70f4f3290545a4c5bb1c2b19f04662e3315f8845
generaluseai <generaluseai@gmail.com>
2021-08-03 20:19:57 +0800

    [impl] FunctionInline refactor

--- src/backend/machinecodes/MCLoad.java
+++ src/backend/machinecodes/MCLoad.java
@@ -54,9 +54,11 @@ public class MCLoad extends MachineCode{
     public String toString(){
         if(addr instanceof VirtualReg && ((VirtualReg)addr).isGlobal()){
 //            CodeGenManager.getInstance().setGlobalInfo(this);
-            String res="\tmovw\t"+dst.getName()+",\t:lower16:"+addr.getName()+"\n";
+            /*String res="\tmovw\t"+dst.getName()+",\t:lower16:"+addr.getName()+"\n";
             res+="\tmovt\t"+dst.getName()+",\t:upper16:"+addr.getName()+"\n";
-            return res;
+            return res;*/
+            CodeGenManager.getInstance().setGlobalInfo(this);
+            return "";
 //            "\tldr\t"+dst.getName()+",\t="+addr.getName()+"\n"
         }
         String res="\tldr"+ condString(cond)+"\t"+dst.getName()+",\t["+addr.getName();
