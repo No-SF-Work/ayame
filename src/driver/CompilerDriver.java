1334168a4e92db9120af127dfe7d7ae9fa4a397f
Codevka <1724707236@qq.com>
2021-08-07 17:02:47 +0800

    all open

--- src/driver/CompilerDriver.java
+++ src/driver/CompilerDriver.java
@@ -96,7 +96,7 @@ public class CompilerDriver {
       pm.openedPasses_.add("Mem2reg");
       pm.openedPasses_.add("RegAlloc");
       if (Config.getInstance().isO2) {
-//        pm.openedPasses_.add("gvlocalize");
+        pm.openedPasses_.add("gvlocalize");
         pm.openedPasses_.add("branchOptimization");
         pm.openedPasses_.add("emitllvm");
         pm.openedPasses_.add("gvngcm");
