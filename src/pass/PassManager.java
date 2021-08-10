13f995c8c89fa7fc127bfa1e30a84eade9f65395
Codevka <1724707236@qq.com>
2021-08-11 00:16:45 +0800

    change pass order to inline -> unroll

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -31,6 +31,11 @@ public class PassManager {
     irPasses.add(new BranchOptimization());
     irPasses.add(new GVNGCM());
 
+    irPasses.add(new FunctionInline());
+    irPasses.add(new BranchOptimization());
+    irPasses.add(new GVNGCM(true));
+    irPasses.add(new DeadCodeEmit());
+
     irPasses.add(new LoopInfoFullAnalysis());
     irPasses.add(new LCSSA());
     irPasses.add(new ConstantLoopUnroll());
@@ -52,11 +57,6 @@ public class PassManager {
     irPasses.add(new EmitLLVM("afterTwiceUnroll.ll"));
     irPasses.add(new BranchOptimization());
     irPasses.add(new GVNGCM());
-
-    irPasses.add(new FunctionInline());
-    irPasses.add(new BranchOptimization());
-    irPasses.add(new GVNGCM(true));
-    irPasses.add(new DeadCodeEmit());
 
     irPasses.add(new LoopInfoFullAnalysis());
     irPasses.add(new EmitLLVM());
