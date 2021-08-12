26377cc8871c10a311cfbdfedb9b00583cc43061
Codevka <1724707236@qq.com>
2021-08-12 19:05:29 +0800

    merge last break run on function

--- src/pass/ir/LoopMergeLastBreak.java
+++ src/pass/ir/LoopMergeLastBreak.java
@@ -1,15 +1,22 @@
 package pass.ir;
 
+import ir.Analysis.LoopInfo;
+import ir.Loop;
 import ir.MyFactoryBuilder;
 import ir.MyModule;
+import ir.values.Function;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.logging.Logger;
 import pass.Pass.IRPass;
+import util.LoopUtils;
 import util.Mylogger;
 
 public class LoopMergeLastBreak implements IRPass {
 
   private static final Logger log = Mylogger.getLogger(IRPass.class);
   private static final MyFactoryBuilder factory = MyFactoryBuilder.getInstance();
+  private LoopInfo currLoopInfo;
 
   @Override
   public String getName() {
@@ -20,6 +27,30 @@ public class LoopMergeLastBreak implements IRPass {
   public void run(MyModule m) {
     log.info("Running pass : LoopMergeLastBreak");
 
+    for (var funcNode : m.__functions) {
+      var func = funcNode.getVal();
+      if (!func.isBuiltin_()) {
+        runOnFunction(func);
+      }
+    }
+  }
+
+  public void runOnFunction(Function func) {
+    Queue<Loop> loopQueue = new LinkedList<>();
+    this.currLoopInfo = func.getLoopInfo();
+
+    // run on loop manager
+    for (var topLoop : this.currLoopInfo.getTopLevelLoops()) {
+      LoopUtils.addLoopToQueue(topLoop, loopQueue);
+    }
+    while (!loopQueue.isEmpty()) {
+      var loop = loopQueue.remove();
+      runOnLoop(loop);
+    }
+  }
+
+  public void runOnLoop(Loop loop) {
     // TODO 需要递归判断无用返回值
+
   }
 }
