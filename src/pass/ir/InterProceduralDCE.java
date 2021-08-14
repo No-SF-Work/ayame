d2be783fada28c2441705bd76f2f56ac2bd7f3d4
generaluseai <generaluseai@gmail.com>
2021-08-14 21:55:31 +0800

    [patch] remove dead globalVariable

--- src/pass/ir/InterProceduralDCE.java
+++ src/pass/ir/InterProceduralDCE.java
@@ -4,7 +4,12 @@ import ir.MyModule;
 import ir.Use;
 import ir.values.Constants.ConstantInt;
 import ir.values.Function;
+import ir.values.GlobalVariable;
 import ir.values.Value;
+import ir.values.instructions.Instruction;
+import ir.values.instructions.MemInst.GEPInst;
+import ir.values.instructions.MemInst.StoreInst;
+import ir.values.instructions.TerminatorInst.BrInst;
 import ir.values.instructions.TerminatorInst.CallInst;
 import ir.values.instructions.TerminatorInst.RetInst;
 import java.util.ArrayList;
@@ -54,8 +59,101 @@ public class InterProceduralDCE implements IRPass {
     do {
       changed = false;
       removeUseLessRet();
+      removeUselessGV();
     } while (changed);
   }
+
+  //我们认为只有通过use关系向下搜索能够搜索到putch,putarray,putint,br,ret,call的全局变量是"有用"的全局变量
+  //对于ret,跟进分析返回值与传入参数的使用情况(麻)
+  //对于"没用"的全局变量，保留他们的getint getch getarray(虽然应该没有出题人犯病输入一堆没用的数据，但只要有，那就赚了)
+  private void removeUselessGV() {
+    for (GlobalVariable __globalVariable : m.__globalVariables) {
+      analyseGv(__globalVariable);
+      if (happy) {
+        relatedValues.forEach(value -> {
+          if (value instanceof Instruction) {
+            ((Instruction) value).CORemoveAllOperand();
+            ((Instruction) value).node.removeSelf();
+          }
+        });
+      }
+    }
+  }
+
+  private void funcArgsPreAnalyse() {
+
+  }
+
+  private ArrayList<Function> isUsefulInFunc = new ArrayList<>();
+  HashSet<Value> relatedValues = new HashSet<>();
+  ArrayList<RetInst> relatedRet = new ArrayList<>();
+  ArrayList<CallInst> relatedCall = new ArrayList<>();
+  ArrayList<BrInst> relatedBr = new ArrayList<>();
+  HashSet<Function> relatedFunc = new HashSet<>();
+  boolean happy = true;
+
+  private void analyseGv(GlobalVariable gv) {//
+    happy = true;
+    relatedValues.clear();
+    relatedRet.clear();
+    relatedCall.clear();
+    relatedBr.clear();
+    findRelatedUsers(gv);
+
+    for (Function f : funcs) {
+      if (relatedFunc.contains(f)) {
+        happy = false;
+        return;
+      }
+    }
+    if (!relatedBr.isEmpty()) {
+      happy = false;
+      return;
+    }
+    if (!relatedFunc.isEmpty()) {
+      happy = false;
+      return;
+    }
+    if (!relatedCall.isEmpty()) {
+      happy = false;
+      return;
+    }
+
+
+  }
+
+  private void findRelatedUsers(Value value) {
+    if (relatedValues.contains(value)) {
+      return;
+    }
+    if (value instanceof StoreInst) {//store要把user的两个operand都放进去
+      /*
+       * store @a pointer
+       * a和pointer有关联
+       * */
+      if (((StoreInst) value).getPointer() instanceof GEPInst) {
+        findRelatedUsers(((GEPInst) ((StoreInst) value).getPointer()).getAimTo());
+        findRelatedUsers(((StoreInst) value).getVal());
+      }
+
+    }
+    if (value instanceof RetInst) {
+      relatedRet.add((RetInst) value);
+    }
+    if (value instanceof CallInst) {
+      relatedFunc.add(((CallInst) value).getFunc());
+      relatedCall.add((CallInst) value);
+    }
+    if (value instanceof BrInst) {
+      relatedBr.add((BrInst) value);
+    }
+
+    relatedValues.add(value);
+    value.getUsesList().forEach(use -> {
+      findRelatedUsers(use.getUser());
+    });
+  }
+
 
   /*
           x没用但是函数内的dce消不掉
@@ -75,6 +173,8 @@ public class InterProceduralDCE implements IRPass {
           * }
           * */
   //找到返回值没用的函数，把所有返回值改成ret0
+  //remove use less ret 和inter procedural gv dce 组合起来后
+  //实际上能够起到跨过程的局部变量的dce的作用，因为一个局部变量，能够和调用者交流的方式只有gv或者是ret
   private void removeUseLessRet() {
     for (INode<Function, MyModule> fnd : m.__functions) {
       var fun = fnd.getVal();
