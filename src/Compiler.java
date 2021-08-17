a74230a0626ff8aef729ba62f8c76a6278285c7e
roife <roifewu@gmail.com>
2021-08-17 13:43:06 +0800

    remove driver

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,9 +1,8 @@
 import driver.CompilerDriverRaw;
-import driver.CompilerDriver;
 
 public class Compiler {
 
   public static void main(String[] args) {
-    CompilerDriver.run(args);
+    CompilerDriverRaw.run(args);
   }
 }
