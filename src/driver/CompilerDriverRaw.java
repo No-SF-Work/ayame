ff2d3db873f9196aeecdee7f3afa0f61089df0f0
roife <roifewu@gmail.com>
2021-08-17 13:43:22 +0800

    for local

--- src/driver/CompilerDriverRaw.java
+++ src/driver/CompilerDriverRaw.java
@@ -93,11 +93,11 @@ public class CompilerDriverRaw {
         pm.openedPasses_.add("constantLoopUnroll");
         pm.openedPasses_.add("MergeMachineBlock");
         pm.openedPasses_.add("redundantLoop");
-//        pm.openedPasses_.add("loopIdiom");
+        pm.openedPasses_.add("loopIdiom");
         pm.openedPasses_.add("markParallel");
         pm.openedPasses_.add("loopMergeLastBreak");
         pm.openedPasses_.add("promotion");
-//        pm.openedPasses_.add("loopFusion");
+        pm.openedPasses_.add("loopFusion");
       }
 
       pm.runIRPasses(MyModule.getInstance());
