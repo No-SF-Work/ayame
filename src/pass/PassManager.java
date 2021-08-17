2327c88460052165c99e2ba0b81d8f917fae67c8
roife <roifewu@gmail.com>
2021-08-17 15:08:28 +0800

    fix for median

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -88,7 +88,7 @@ public class PassManager {
     irPasses.add(new GVNGCM(true));
 
     irPasses.add(new LCSSA());
-    irPasses.add(new BranchOptimization());
+//    irPasses.add(new BranchOptimization());
     irPasses.add(new GVNGCM(true));
     irPasses.add(new LoopInfoFullAnalysis());
     irPasses.add(new EmitLLVM());
